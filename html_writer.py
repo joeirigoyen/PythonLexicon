69b72d36002fa2bdff072d55701dc23113883ef3
Youthan Irigoyen <a01750476@itesm.mx>
2021-05-02 22:13:06 -0500

    Update html_writer.py

--- html_writer.py
+++ html_writer.py
@@ -1,7 +1,38 @@
-def openFile(filename):
-    f = filename + ".html"
-    open(f, "w")
+import os
+import re
 
 
-for i in range(0, 5):
-    print("stupid")
+# Initialize html file
+def init_file(filename):
+    # Get absolute directory for test files
+    file_dir = os.path.abspath("test_files")
+    # Open file in write mode
+    f = open(filename, "w+")
+    # Close file
+    f.close()
+    # Write initial html tags
+    init_html(file_dir + "\\" + filename)
+    return f
+
+
+# Write initial html tags including css stylesheet linking
+def init_html(filename):
+    # Open file in write mode
+    f = open(filename, "w+")
+    # Set initial tags
+    init_tags = '''<html>
+    <head>
+        <link rel=\"stylesheet\" href=\"styles.css\">
+    </head>'''
+    # Write initial tags to file
+    f.write(init_tags)
+    # Close file
+    f.close()
+
+
+# Read input file
+def read_file(filename):
+
+
+
+test_file = init_file("test.html")
